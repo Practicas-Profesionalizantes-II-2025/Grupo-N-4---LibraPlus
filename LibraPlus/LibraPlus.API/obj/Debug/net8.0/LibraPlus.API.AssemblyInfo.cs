42972357009c1f49c678eabd2b0704fbb847239b
Mateo Ferrero <mateo.ferrero.ices@gmail.com>
2025-10-19 16:15:54 -0300

    separacion de roles funcional

--- LibraPlus/LibraPlus.API/obj/Debug/net8.0/LibraPlus.API.AssemblyInfo.cs
+++ LibraPlus/LibraPlus.API/obj/Debug/net8.0/LibraPlus.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraPlus.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271fa36247a5e877fe460289c604167092cb8cd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+723ac90ea418d8c58ec7d1ce50f7e19fa5b90c6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraPlus.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraPlus.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
